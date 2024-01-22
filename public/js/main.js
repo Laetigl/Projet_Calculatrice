c5d7e266844c980b04e409dd9b5e9905a0b1bcb9
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-01-22 09:47:23 +0100

    Frame and begining styling

---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+let operations = ["+","-","x","/","="]
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let frameSection = document.querySelector("section")
+// let sectionOperator = document.createElement("div")
+// sectionOperator.style.width="100px"
+// sectionOperator.style.display="flex"
+// //Buttons for operations
+// let buttonsOp
+
+// for (let index = 0; index < 5; index++) {
+//     buttonsOp = document.createElement("div")  
+//     sectionOperator.appendChild(buttonsOp)
+//     buttonsOp.classList="operators"
+
+// }
+// buttonsOp=document.querySelectorAll(".operators")
+
+// buttonsOp.forEach(element => {
+//     element.style.border="1px solid black"
+//     element.style.width="60px"
+//     element.style.height="60px"
+//     // element.style.display="flex"
+//     element.style.justifyContent="space-around"
+//     element.style.gap="10px"
+
+// })
+
+// console.log(buttonsOp);
+// //Buttons for numbers
+// let buttonsNb
+// for (let index = 0; index < 10; index++) {
+//     buttonsNb = document.createElement("div")
+//     buttonsNb.classList="numbers"
+//     frameSection.appendChild(buttonsNb) 
+// }
+// buttonsNb = document.querySelectorAll(".numbers")
+
+// buttonsNb.forEach(element => {
+//     element.style.border="1px solid black"
+//     element.style.width="60px"
+//     element.style.height="60px"
+//     element.style.display="flex"
+//     element.style.justifyContent="space-around"
+//     element.style.gap="10px"
+// })
